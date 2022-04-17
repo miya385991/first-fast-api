9f753a6d7f8a03bcd8712dd62534917086734351
yusuke <38mtyuusuuke@gmail.com>
2022-04-17 23:48:05 +0900

    db 接続 & post

--- main.py
+++ main.py
@@ -1,13 +1,45 @@
+import sqlite3
 from fastapi import FastAPI
 
+
+def create_tebles():
+    conn = sqlite3.connect('test.db')
+    curs = conn.cursor()
+    curs.execute(
+        'CREATE TABLE users(id INTEGER PRIMARY KEY AUTOINCREMENT, name STRING)'
+    )
+    conn.commit()
+    conn.close()
+
+
+
 app = FastAPI()
+def get_db():
+    conn = sqlite3.connect('test.db')
+    curs = conn.cursor()
+    curs.execute('SELECT * FROM users')
+    users = curs.fetchall()
+    conn.close()
+    return users
 
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    user = {}
+    users = get_db()
+    for id, name in users:
+        user[id] = name
+    return user
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}
+@app.post('/')
+def post_name(name: str):
+    conn = sqlite3.connect('test.db')
+    curs = conn.cursor()
+    db_text = f'INSERT INTO users(name) values("{name}")'
+    curs.execute(db_text)
+    conn.commit()
+    conn.close()
+
+
+
